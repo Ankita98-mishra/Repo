476a946efb5ebdf69903b6e8620e9732470d9d69
ankita <ankita123@gmail.com>
2022-09-01 21:57:17 +0530

    added

---
+++ challenge2.tf
@@ -0,0 +1,5 @@
+Challenge #2
+We need to write code that will query the meta data of an instance within AWS and provide a json formatted output. The choice of language and implementation is up to you.
+
+
+aws <resource> describe-resource --resource-id <resource-id> --output json
